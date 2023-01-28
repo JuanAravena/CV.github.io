74206dca27161def235f3584053e81d97b887aab
Juan Pablo Aravena <juampiaravena@gmail.com>
2023-01-28 14:41:13 -0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,8 @@
+
+document.getElementById('boton_fondo').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#000000';
+});
+document.getElementById('boton_fondo1').addEventListener('click',function() {
+    document.body.style.backgroundColor = '#f5f1ec';
+});
+        
